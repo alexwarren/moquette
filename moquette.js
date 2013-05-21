55fa1d3346708f3b0caf4a36d81e1daf9534a4c5
Alex Warren <alex@axeuk.com>
2013-05-21 17:42:16 +0100

    Better way of doing that

--- moquette.js
+++
@@ -1,7 +0,0 @@
-function moquetteResetScroll() {
-	setTimeout(function(){
-		$('html, body').animate({ scrollTop: scrollTo }, 200);
-		markScrollPosition();
-		$("#divOutput").css("min-height", 0);
-	}, 250);
-}
