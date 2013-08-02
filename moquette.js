286419760fa6a1095dddd1ee36900d2205970cb9
Alex Warren <alex@axeuk.com>
2013-08-02 14:23:10 +0100

    Nicer animation

--- moquette.js
+++ moquette.js
@@ -1,29 +1,52 @@
 ﻿var _heatherTextCount = 0;
 
 function heatherText() {
+	$("<div/>", {
+		id: "heatherText"
+	}).css({
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		overflow: "hidden"
+	}).appendTo("body");
+	doHeatherText();
+}
+
+function doHeatherText() {
 	_heatherTextCount++;
 	if (_heatherTextCount < 150) {
 		setTimeout(function() {
-			heatherText();
-		}, 30);
+			doHeatherText();
+		}, 20);
 	}
 	else {
-		ASLEvent("JSFinish_HeatherText", "");
+		$("#heatherText").fadeOut(2000);
+		setTimeout(function() {
+			ASLEvent("JSFinish_HeatherText", "");
+		}, 500);
 	}
 	var maxLeft = $(window).width();
 	var maxTop = $(window).height();
 	$("<div/>", {
-		"class": "temp",
 		text: "Heather"
 	}).css({
 		position: "absolute",
 		left: Math.random() * maxLeft,
 		top: Math.random() * maxTop,
 		"font-size": getRandomInt(8, 100) + "px",
-		"font-family": "Georgia, serif"
-	}).appendTo("body");
+		"font-family": "Georgia, serif",
+		color: getRandomGrey(),
+		opacity: Math.random(),
+	}).appendTo("#heatherText");
 }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomGrey() {
+	var hex = Math.floor(Math.random() * 256).toString(16);
+	return "#" + hex + hex + hex;
 }
