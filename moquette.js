f27c312d8a1e25e3a6c04cf325ecd40d7f226bdf
Alex Warren <alex@axeuk.com>
2013-08-02 14:14:27 +0100

    An animation

--- moquette.js
+++ moquette.js
@@ -1,6 +1,29 @@
-﻿function heatherText() {
-	addText("displaying text...");
-	setTimeout(function(){
+﻿var _heatherTextCount = 0;
+
+function heatherText() {
+	_heatherTextCount++;
+	if (_heatherTextCount < 150) {
+		setTimeout(function() {
+			heatherText();
+		}, 30);
+	}
+	else {
 		ASLEvent("JSFinish_HeatherText", "");
-	}, 2000);
+	}
+	var maxLeft = $(window).width();
+	var maxTop = $(window).height();
+	$("<div/>", {
+		"class": "temp",
+		text: "Heather"
+	}).css({
+		position: "absolute",
+		left: Math.random() * maxLeft,
+		top: Math.random() * maxTop,
+		"font-size": getRandomInt(8, 100) + "px",
+		"font-family": "Georgia, serif"
+	}).appendTo("body");
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
